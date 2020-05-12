a93eb178a38519f18ac8c7c54dc2bc778aa2c8b1
Patrick O'Neill <po73@drexel.edu>
2020-05-12 02:31:00 -0400

    Fixed Quiz Answer Key again

--- server/routes/quiz.js
+++ server/routes/quiz.js
@@ -129,7 +129,25 @@ quizRouter.post('/gradequiz2', permissions.isUserAlreadyLogedIn, permissions.isU
 });
 
 quizRouter.get('/quiz2_key', permissions.isUserAlreadyLogedIn, permissions.isUserTeacher, async (req, res) => {
+    var userDash = null;
+    try{
+        userDash = await navbar.setNavBar(req.sessionID); //Determine which menu bar should be loaded
+    }
+    catch (error) {
+        console.log(error);
+    }
+    const QuizID = 2;
 
+    const displayElements = await quiz.generateTeacherQuiz(QuizID);
+    console.log(displayElements);
+
+    if(userDash){
+        res.render('./Quiz_Pages/keyQuiz', {userDash, Title: displayElements.StudentView.Title, displayThis: displayElements.StudentView.Section, 
+            answerKey: displayElements.TeacherQuestions.Section, keyAnswers: displayElements.TeacherQuestionKey});
+    }
+    else{
+        res.redirect('/');
+    }
 });
 
 quizRouter.get('/quiz3', permissions.isUserAlreadyLogedIn, permissions.isUserStudent, async (req, res) => {
@@ -182,7 +200,25 @@ quizRouter.post('/gradequiz3', permissions.isUserAlreadyLogedIn, permissions.isU
 });
 
 quizRouter.get('/quiz3_key', permissions.isUserAlreadyLogedIn, permissions.isUserTeacher, async (req, res) => {
+    var userDash = null;
+    try{
+        userDash = await navbar.setNavBar(req.sessionID); //Determine which menu bar should be loaded
+    }
+    catch (error) {
+        console.log(error);
+    }
+    const QuizID = 3;
 
+    const displayElements = await quiz.generateTeacherQuiz(QuizID);
+    console.log(displayElements);
+
+    if(userDash){
+        res.render('./Quiz_Pages/keyQuiz', {userDash, Title: displayElements.StudentView.Title, displayThis: displayElements.StudentView.Section, 
+            answerKey: displayElements.TeacherQuestions.Section, keyAnswers: displayElements.TeacherQuestionKey});
+    }
+    else{
+        res.redirect('/');
+    }
 });
 
 module.exports = quizRouter;
